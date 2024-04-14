7ae9a45cb9509a798076ed1474a95686650cef27
darmike <darmogray777@gmail.com>
2024-04-14 14:15:11 +0300

    les3 commit

---
+++ les3/Truck.php
@@ -0,0 +1,14 @@
+<?php
+
+class Truck extends Car {
+    private float $maxLoadCapacity;
+
+    public function __construct(string $brand, string $model, int $year, float $maxLoadCapacity) {
+        parent::__construct($brand, $model, $year);
+        $this->maxLoadCapacity = $maxLoadCapacity;
+    }
+
+    public function getMaxLoadCapacity(): float {
+        return $this->maxLoadCapacity;
+    }
+}
