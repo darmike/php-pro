a5d7f1567e668f00800ac59f2ae3b6ed7b5a473c
darmike <darmogray777@gmail.com>
2024-05-12 23:10:52 +0300

    les9

--- les9/sql_query.sql
+++ les9/sql_query.sql
@@ -1,13 +1,17 @@
 
-
+//mysql
 
 CREATE INDEX author_first_name_last_name ON author (first_name, last_name);
+
 CREATE INDEX book_author_id ON book (author_id);
 
 SELECT author.id, author.first_name, author.last_name, COUNT(book.id) AS book_count
+
 FROM author
          JOIN book ON author.id = book.author_id
+
 WHERE author.first_name = 'Zaria' AND author.last_name = 'Barton'
+
 GROUP BY author.id, author.first_name, author.last_name;
 
 
