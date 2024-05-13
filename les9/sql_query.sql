929ec1394d9c85f8865ce0d9be7d96a9a36c28ee
darmike <darmogray777@gmail.com>
2024-05-13 13:39:34 +0300

    with mysql tables

--- les9/sql_query.sql
+++ les9/sql_query.sql
@@ -1,5 +1,5 @@
 
-//mysql
+
 
 CREATE INDEX author_first_name_last_name ON author (first_name, last_name);
 
@@ -14,7 +14,7 @@ WHERE author.first_name = 'Zaria' AND author.last_name = 'Barton'
 
 GROUP BY author.id, author.first_name, author.last_name;
 
-
+# Новий коміт для переходу в гілку мейн
 # Я використав JOIN для з'єднання таблиць 'author' і 'book' за умовою,
 # що author.id дорівнює book.author_id. дозволяє отримати всі книги,
 # які належать автору з іменем "Zaria Barton" без використання додаткового підзапиту.
